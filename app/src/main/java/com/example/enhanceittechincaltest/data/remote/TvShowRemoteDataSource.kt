c2b50a1f856709b605ac257b49229e166613a685
amn <satija.aman@gmail.com>
2023-03-21 06:59:42 +0530

    added in memeory caching to cache in memeory already searched queries

--- app/src/main/java/com/example/enhanceittechincaltest/data/remote/TvShowRemoteDataSource.kt
+++ app/src/main/java/com/example/enhanceittechincaltest/data/remote/TvShowRemoteDataSource.kt
@@ -1,10 +1,24 @@
 package com.example.enhanceittechincaltest.data.remote
+import com.example.enhanceittechincaltest.data.remote.tvshow.ModelResTvShows
 import com.example.enhanceittechincaltest.utils.api.ApiService
+import com.example.enhanceittechincaltest.utils.core.Resource
 import javax.inject.Inject
 
 class TvShowRemoteDataSource @Inject constructor(
     private val apiService: ApiService
 ): BaseDataSource() {
 
-    suspend fun searchTvShows(query: String) = executeRemoteReq { apiService.searchTvShows(query) }
+    var mHmCacheOfResponses = HashMap<String, Resource<ModelResTvShows>>()
+    var boolIsCacheDirty = false;
+    suspend fun searchTvShows(query: String) : Resource<ModelResTvShows> {
+        if(boolIsCacheDirty ||
+            !mHmCacheOfResponses.containsKey(query)){
+            val result = executeRemoteReq { apiService.searchTvShows(query) }
+            mHmCacheOfResponses.set(query,result)
+            return   result;
+        }else{
+            return mHmCacheOfResponses[query]!!
+        }
+
+    }
 }
